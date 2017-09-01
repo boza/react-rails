d9bbb9d8fcbf6bd06fe3e675b5314ebd54a67e57
Jose Boza <jaboza@gmail.com>
2017-09-01 15:39:28 +0200

    Add http fetch utility to make requets through react

--- app/javascript/packs/components/page.js
+++ app/javascript/packs/components/page.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Jumbotron, Button } from 'reactstrap';
 
+const goToLogin = () => window.location = '/sign_in'
+
 const Page = (props) => {
   return (
     <div>
       <Jumbotron>
         <h1 className="display-3">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p className="lead">This is a example application with Rails 5.1, react, redux, bootstrap</p>
         <hr className="my-2" />
-        <p>It uses utility classes for typgraphy and spacing to space content out within the larger container.</p>
         <p className="lead">
-          <Button color="primary">Learn More</Button>
+          <Button onClick={goToLogin} color="primary">Sign In</Button>
         </p>
       </Jumbotron>
     </div>
---
+++ app/javascript/packs/utils/http.js
@@ -0,0 +1,51 @@
+/* eslint-env browser */
+import humps from 'humps'
+import 'whatwg-fetch'
+
+const defaultHeaders = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
+}
+
+const endpoint = '' // Just point to the same host.
+
+const request = (type, url, data = {}) => {
+  const formattedFormData = humps.decamelizeKeys(data)
+  const body = JSON.stringify(formattedFormData)
+
+  return fetch(`${endpoint}${url}`, {
+    method: type,
+    headers: defaultHeaders,
+    body: body,
+    credentials: 'include'
+  }).then(function (response) {
+    if (response.ok) {
+      return response.json().then(json => json)
+    } else {
+      return response.json().then(e => {
+        throw e
+      })
+    }
+  })
+}
+
+export default {
+  post: (url, data = {}) => {
+    return request('POST', url, data)
+  },
+  put: (url, data = {}) => {
+    return request('PUT', url, data)
+  },
+  get: (url, data = {}) => {
+    return fetch(`${endpoint}${url}`, {
+      method: 'GET',
+      headers: defaultHeaders
+    }).then(function (response) {
+      if (response.ok) {
+        return response.json().then(json => json.data)
+      } else {
+        return response.json().then(e => { throw e })
+      }
+    })
+  }
+}
